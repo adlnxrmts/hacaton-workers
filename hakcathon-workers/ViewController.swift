b16eed49e8e3db30d4e109b0ae054d881de6f383
adlnxrmts <adelkxr@mail.ru>
2020-10-29 21:29:45 +0300

    Initial Commit

---
+++ hakcathon-workers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hakcathon-workers
+//
+//  Created by Adelina on 29.10.2020.
+//  Copyright © 2020 AdelineHramtz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
